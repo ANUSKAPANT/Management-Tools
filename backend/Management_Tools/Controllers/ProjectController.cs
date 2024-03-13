632ee52f439bd81cfe7faf946170366bc8d09f71
anuskapant <anuska.pant@gmail.com>
2024-03-13 00:28:39 -0500

    controller added for the models

---
+++ backend/Management_Tools/Controllers/ProjectController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Management_Tools.Models;
+using Management_Tools.Data;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.EntityFrameworkCore;
+
+namespace Management_Tools.Controllers
+{
+    [Route("api/Project")]
+    [ApiController]
+    public class ProjectController : Controller
+	{
+        private readonly ApplicationDbContext _db;
+
+        public ProjectController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Project>> GetProjects()
+        {
+            var projects = _db.Projects
+                .Include(p => p.Todos)
+                .ThenInclude(p => p.Users)
+                    .Include(t => t.Users) // Include related Users for each Todo
+                .ToList();
+
+
+            return Ok(projects);
+        }
+
+
+        [HttpPost]
+        public ActionResult<Project> CreateProject([FromBody] Project project)
+        {
+            if (project == null) return BadRequest();
+            if (project.Id > 0) return StatusCode(StatusCodes.Status500InternalServerError);
+
+            Project newProject = new Project
+            {
+
+                ProjectName = project.ProjectName,
+                ProjectDescription = project.ProjectDescription,
+                Todos = project.Todos
+            };
+
+            _db.Projects.Add(newProject);
+            _db.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpGet("id")]
+        public ActionResult<Project> GetProject(int id)
+        {
+            if (id == 0) return BadRequest();
+            var project = _db.Projects.FirstOrDefault(proj => proj.Id == id);
+            if (project == null)
+                return NotFound();
+
+            return Ok(project);
+        }
+
+        [HttpDelete("id")]
+        public IActionResult DeleteProject(int id)
+        {
+            if (id == 0) return BadRequest();
+            var project = _db.Projects.FirstOrDefault(proj => proj.Id == id);
+            if (project == null)
+                return NotFound();
+            _db.Projects.Remove(project);
+            _db.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPut("id")]
+        public IActionResult UpdateProject(int id, [FromBody] Project project)
+        {
+            if (project == null || project.Id != id) return BadRequest();
+            _db.Projects.Update(project);
+            _db.SaveChanges();
+
+            return NoContent();
+        }
+
+
+
+        [HttpPatch("id")]
+        public IActionResult UpdateProject(int id, JsonPatchDocument<Project> project)
+        {
+            if (project == null || id == 0) return BadRequest();
+            var proj = _db.Projects.FirstOrDefault(u => u.Id == id);
+
+
+            if (proj == null) return BadRequest();
+
+            project.ApplyTo(proj, ModelState);
+
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            _db.Projects.Update(proj);
+            _db.SaveChanges();
+            return NoContent();
+        }
+
+    }
+}
+
---
+++ backend/Management_Tools/Controllers/TodoController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Management_Tools.Models;
+using Management_Tools.Data;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.EntityFrameworkCore;
+
+namespace Management_Tools.Controllers
+{
+    [Route("api/Todo")]
+    [ApiController]
+    public class TodoController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TodoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Todo>> GetTodos()
+        {
+            var todos = _db.Todos.Include(todo=>todo.Project).Include(todo => todo.Users).ToList();
+            return Ok(todos);
+        }
+
+        [HttpPost]
+        public ActionResult<Todo> CreateTodo([FromBody] Todo todo)
+        {
+            if (todo == null) return BadRequest();
+            if (todo.Id > 0) return StatusCode(StatusCodes.Status500InternalServerError);
+
+            Todo newTodo = new Todo
+            {
+                TodoName = todo.TodoName,
+                TodoDescription = todo.TodoDescription,
+                ProjectId = todo.ProjectId,
+            };
+
+            _db.Todos.Add(newTodo);
+            _db.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpGet("id")]
+        public ActionResult<Todo> GetTodo(int id)
+        {
+            if (id == 0) return BadRequest();
+            var Todo = _db.Todos.FirstOrDefault(proj => proj.Id == id);
+            if (Todo == null)
+                return NotFound();
+
+            return Ok(Todo);
+        }
+
+        [HttpDelete("id")]
+        public IActionResult DeleteTodo(int id)
+        {
+            if (id == 0) return BadRequest();
+            var Todo = _db.Todos.FirstOrDefault(proj => proj.Id == id);
+            if (Todo == null)
+                return NotFound();
+            _db.Todos.Remove(Todo);
+            _db.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPut("id")]
+        public IActionResult UpdateTodo(int id, [FromBody] Todo todo)
+        {
+            if (todo == null || todo.Id != id) return BadRequest();
+            _db.Todos.Update(todo);
+            _db.SaveChanges();
+
+            return NoContent();
+        }
+
+
+
+        [HttpPatch("id")]
+        public IActionResult UpdateTodo(int id, JsonPatchDocument<Todo> todo)
+        {
+            if (todo == null || id == 0) return BadRequest();
+            var proj = _db.Todos.FirstOrDefault(u => u.Id == id);
+
+
+            if (proj == null) return BadRequest();
+
+            todo.ApplyTo(proj, ModelState);
+
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            _db.Todos.Update(proj);
+            _db.SaveChanges();
+            return NoContent();
+        }
+
+    }
+}
+
